11be06b3fe17781ea288608caf1a086437ead83e
Tomás Canavidez <tms1csgo@gmail.com>
2022-12-24 14:41:39 -0300

    updating repo

--- 2. Juego Mad Lib/main.py
+++ 2. Juego Mad Lib/main.py
@@ -1,30 +1,24 @@
 print("¡Hola! Bienvenido a MadLib")
 
 print("Vamos a armar una historia, pero debes responder algunas preguntas...")
-loop = 1
 
-while loop > 10:
-    r1 = str(input("Dinos... una profesion para el sujeto: "))
+r1 = str(input("Dinos... una profesion para el sujeto: "))
 
-    print(r1, " Suena bien...")
+print(r1, " Suena bien...")
+r2 = str(input("¿En que ciudad o lugar?: "))
+r3 = str(input("Indica a su pariente mas querido: "))
 
-    r2 = str(input("¿En que ciudad o lugar?: "))
+print("Su ", r3 , "No me lo esperaba...")
 
-    r3 = str(input("Indica a su pariente mas querido: "))
+r4 = str(input("Cuentame que hacia nuestro protagonista: "))
 
-    print("Su ", r3 , "No me lo esperaba...")
+r5 = str(input("¿Cual es el artista favorito del protagonista?: "))
+print()
+print()
 
-    r4 = str(input("Cuentame que hacia nuestro protagonista: "))
+print("Genial! la historia queda asi...")
 
-    r5 = str(input("¿Cual es el artista favorito del protagonista?: "))
-    print()
-    print()
+print()
+print()
 
-    print("Genial! la historia queda asi...")
-
-    print()
-    print()
-
-    print("Habia una vez un ", r1 ," que solia caminar por las calles de " ,r2 , " buscando a su " ,r3 , " mientras " ,r4 , " y cantaba una cancion de " ,r5 , )
-
-    loop = loop + 1.
+print("Habia una vez un ", r1 ," que solia caminar por las calles de " ,r2 , " buscando a su " ,r3 , " mientras " ,r4 , " y cantaba una cancion de " ,r5 , )
---
+++ 3. Contador de palabras/README.md
@@ -0,0 +1,7 @@
+Preguntamos al usuario en que está pensando. Cuando se introduce la respuesta, realizará el conteo de palabras en la sentencia e imprimimos en la salida el resultado.
+
+Ejemplo:
+
+Pregunta: ¿En qué estás pensando?
+Entrada: Bien, hoy es el día en el que me voy a crear un desarrollador experto
+Salida: ¡Muy bien, tu me has mostrado tu pensamiento en 15 palabras!
